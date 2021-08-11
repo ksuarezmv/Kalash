bf86438ec4d84aa12c5474cb521ceb9265ca955a
ksuarezmv <76536179+ksuarezmv@users.noreply.github.com>
2021-08-11 14:58:44 -0500

    Add files via upload

---
+++ creadores.php
@@ -0,0 +1,353 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Creadores</title>
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+		<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+		<script src="https://kit.fontawesome.com/2c36e9b7b1.js"></script>
+		<link rel="stylesheet" href="./css/estilos.css">
+		<link rel="stylesheet" href="./animaciones/anijs-master/animaciones.css">
+		<link rel="stylesheet" href="./css/animaciones.css">
+		<link rel="stylesheet" href="https://unpkg.com/aos@next/dist/aos.css" />
+</head>
+<body>
+	<nav class="navbar sticky-top navbar-expand-lg navbar-dark">
+		<a class="navbar-brand ml-lg-5" href="#">Kalash</a>
+		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#menu4" aria-controls="menu4" aria-expanded="false" aria-label="Toggle navigation">
+			<span class="navbar-toggler-icon"></span>
+		</button>
+	
+		<div class="collapse navbar-collapse" id="menu4">
+			<ul class="navbar-nav mx-auto">
+				<li class="nav-item active"><a class="nav-link" href="index.html">Inicio<span class="sr-only">(current)</span></a></li>
+				<li class="nav-item"><a class="nav-link" href="blog.html">Blog</a></li>
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="submenu4" role="button" data-toggle="dropdown" 
+					aria-haspopup="true" aria-expanded="false">Exposición</a>
+					<div class="dropdown-menu" aria-labelledby="submenu4">
+						<a class="dropdown-item" href="escritores.html">Escritores</a>
+						<a class="dropdown-item" href="creadores.php">Creadores</a>
+						<a class="dropdown-item" href="poetas.html">Poetas</a>
+					</div>
+				</li>
+				<li class="nav-item"><a class="nav-link" href="galeria.html">Galeria</a></li>
+				<li class="nav-item"><a class="nav-link" href="contacto.html">Contacto</a></li>
+			</ul>
+			<div class="navbar-text mr-lg-5" style="font-size: 1rem; letter-spacing: 2px;">
+				<a href="https://www.facebook.com/kalashblog/" class="pe-1"><i class="fab fa-facebook"></i></a>
+				<a href="https://www.instagram.com/kalashblog/" class="pe-1"><i class="fab fa-instagram"></i></a>
+				<a href="https://co.pinterest.com/kalashblog/_saved/" class="pe-1"><i class="fab fa-pinterest"></i></a>
+				<a href="https://kalashmyblog.tumblr.com" class="pe-1"><i class="fab fa-tumblr"></i></a>
+			</div>
+		</div>
+	</nav>
+	
+  <div class="container-fluid g-0" id="escritores-0">
+    <div class="row" id="creadores-o2">
+			<div class="col-sm-12 p-0">
+        <h1>Creadores</h1>
+				<h2>❝Mientras sea creador, por bajo que sea el nivel de su creación, un hombre puede considerarse realmente libre.❞</h2>
+			</div>
+    </div>
+  </div>
+
+	<div class="contenedor">
+		<div class="elemento" id="small" style="background-color: #fcce22;">
+			<h1>❝ ❞</h1>
+			<p>❝Ya lo acepté, pero aún me pone triste. ❞ </p>	
+		</div>
+		<div class="elemento" id="small" style="background-color: #2887bf;">
+			<h1>❝ ❞</h1>
+			<p>❝Mientras el corazón late, mientras el cuerpo y alma siguen juntos, no puedo admitir que cualquier criatura dotada de voluntad tiene necesidad de perder la esperanza en la vida.❞</p>
+			<h2>Julio Verne</h2>			
+		</div>
+		<div class="elemento" id="small" style="background-color: #935db1;">
+			<h1>❝ ❞</h1>
+			<p>❝La naturaleza puede ser corregida, enmendada, pues de no ser así quedaríamos sepultados bajo los prejuicios. Sin eso no habría ni un solo gran hombre.❞</p>
+			<h2>Fiodor Dostoievski</h2>			
+		</div>
+		<div class="elemento" id="small" style="background-color: #fb8d39;">
+			<h1>❝ ❞</h1>
+			<p>❝Hasta que no tengan conciencia de su fuerza, no se rebelarán, y hasta después de haberse revelado, no serán conscientes. Ese es el problema.❞</p>
+			<h2>George Orwell</h2>
+		</div>
+		<div class="elemento" id="small" style="background-color: #df5352;">
+			<h1>Cielo para tus alas</h1>
+			<p>❝ Usted no va a encajar en ningún sitio si vive esperando lo mismo que usted da... amor, respeto, cariño, verdad, luz en fin, no todos sabemos ser tan buenos devolviendo lo que nos dan pero usted sea feliz dando. Ese es el sentido de la vida que al morir pueda saber que se va con las manos vacías y el corazón lleno, porque lo dio todo.❞ </p>
+			<h2>Fredy Jiménez</h2>	
+		</div>
+		<div class="elemento" id="small" style="background-color: #ff4076;">
+			<h1>Ella y su magia.</h1>
+			<p>❝ A veces se viste con su locura, sonríe mientras recuerda y baila con su desastre. A veces es fuerte y otras veces llora; a veces tiene miedo y otras veces se enamora. A veces solo quiere detenerse, apagarse un poco y rendirse ante sus heridas; otras veces quiere intentarlo hasta el final y amarse sin medida. Asi es ella, siempre tan libre, tan valiente y tan bella; tan de sí misma y tan de nadie.❞ </p>
+			<h2>J. Wailen</h2>		
+		</div>
+		<div class="elemento" id="small" style="background-color: #ad4d30;">
+			<h1>❝ ❞</h1>
+			<p>❝Lo que sólo ocurre una vez es como si no ocurriera nunca. Si el hombre sólo puede vivir una vida es como si no viviera en absoluto.❞</p>
+			<h2>Milan Kundera</h2>
+		</div>
+		<div class="elemento" id="small" style="background-color: #02aa6d;">
+			<h1>❝ ❞</h1>
+			<p>❝¿Qué es la vida? Un frenesí. ¿Qué es la vida? Una ilusión, una sombra, una ficción; y el mayor bien es pequeño; que toda la vida es sueño, y los sueños, sueños son.❞</p>
+			<h2>Calderón de la Barca</h2>
+		</div>
+		<div class="elemento" id="small" style="background-color: #fcce22;">
+			<h1>❝ ❞</h1>
+			<p>❝El café ayuda a quien duerme poco y sueña mucho.❞ </p>
+		</div>
+		<div class="elemento" id="small" style="background-color: #2887bf;">
+			<h1>❝ ❞</h1>
+			<p>❝La vida de los baños de mar y la vida de viajes me hacen ver que el teatro del mundo dispone de menos decorados que de actores y de menos actores que de situaciones.❞</p>
+			<h2>Marcel Proust</h2>	
+		</div>
+		<div class="elemento" id="small" style="background-color: #935db1;">
+			<h1>❝ ❞</h1>
+			<p>❝ Conquistame la mente y la ropa cae sola. ❞ </p>		
+		</div>
+		<div class="elemento" id="small" style="background-color: #fb8d39;">
+			<h1>❝ ❞</h1>
+			<p>❝Yo amo como me gustaría que me amaran. ❞ </p>
+		</div>
+		<div class="elemento" id="small" style="background-color: #df5352;">
+			<h1>Mi peor versión</h1>
+			<p>❝Soy enemigo de las primeras impresiones, por eso suelo mostrar mi peor versión, es algo así como una “prueba”, no me interesa estar con una persona que se espanta con esa parte de mí, solo aquella que logra aceptar mi peor versión, solo se merece lo mejor de mí, se merece todo. ❞ </p>
+			<h2>Andrés Ixtepan</h2>	
+		</div>
+		<div class="elemento" id="small" style="background-color: #ff4076;">
+			<h1>❝ ❞</h1>
+			<p>❝Que el destino nos encuentre en todas las vidas. ❞ </p>		
+		</div>
+		<div class="elemento" id="small" style="background-color: #ad4d30;">
+			<h1>❝ ❞</h1>
+			<p>❝La mente es su propio lugar, y en sí misma puede hacer un cielo de infierno, o un infierno de cielo.❞</p>
+			<h2>John Milton</h2>
+		</div>
+		<div class="elemento" id="small" style="background-color: #02aa6d;">
+			<h1>❝ ❞</h1>
+			<p>❝ Nunca dejes de enloquecerme, ‹lo necesito›❞ </p>
+		</div>
+		<div class="elemento" id="small" style="background-color: #fcce22;">
+			<h1>❝ ❞</h1>
+			<p>❝Qué maravilloso es que nadie necesite esperar ni un sólo momento antes de comenzar a mejorar el mundo.❞</p>
+			<h2>Ana Frank</h2>
+		</div>
+		<div class="elemento" id="small" style="background-color: #2887bf;">
+			<h1>❝ ❞</h1>
+			<p>❝Yo amo como me gustaría que me amaran. ❞ </p>
+		</div>
+		<div class="elemento" id="small" style="background-color: #935db1;">
+			<h1>❝ ❞</h1>
+			<p>❝Señor, las tristezas no se hicieron para las bestias, sino para los hombres;
+				pero si los hombres las sienten demasiado, se vuelven bestias.❞</p>
+			<h2>Miguel de Cervantes</h2>
+		</div>
+		<div class="elemento" id="small" style="background-color: #fb8d39;">
+			<h1>❝ ❞</h1>
+			<p>❝Suerte: es estar contigo. ❞ </p>
+		</div>
+		<div class="elemento" id="small" style="background-color: #df5352;">
+			<h1>A mi edad</h1>
+			<p>❝A mi edad ya conozco de ausencias y promesas que duran medio segundo, de palabras dulces que van atando hasta encerrar el alma.
+				Creo en la magia de los besos, y sé que el complemento llega para revolucionar toda nuestra existencia,	no para opacar la alegría, así que no trates de impresionar, porque para el amor todavía estoy pero para las dudas ya no❞ </p>
+			<h2>Isidro Sánchez</h2>			
+		</div>
+		<div class="elemento" id="small" style="background-color: #ff4076;">
+			<h1>❝ ❞</h1>
+			<p>❝Te perdí, pero te encuentro en cada recuerdo. ❞ </p>
+		</div>
+		<div class="elemento" id="small" style="background-color: #ad4d30;">
+			<h1>❝ ❞</h1>
+			<p>❝La tierra va rotando sobre su eje. Y, sin ninguna relación con ello, todos nosotros vivimos dentro de un sueño.❞</p>
+			<h2>Haruki Murakami</h2>
+		</div>
+		<div class="elemento" id="small" style="background-color: #02aa6d;">
+			<h1>❝ ❞</h1>
+			<p>❝ Ahora hay que vivir hasta quemarse.❞ </p>
+		</div>
+		<div class="elemento" id="small" style="background-color: #fcce22;">
+			<h1>❝ ❞</h1>
+			<p>❝La vida es más amplia que lo que nuestras razones y conveniencias creen y la misión de la literatura es devolvernos esas posibilidades incumplidas.❞</p>
+			<h2>Gustavo Martín Garzo</h2>
+		</div>
+		<div class="elemento" id="small" style="background-color: #2887bf;">
+			<h1>❝ ❞</h1>
+			<p>❝La poesía no tiene tiempo, el que la lee la rescata, la hace presente y luego la regresa a su eternidad.❞</p>
+			<h2>Doménico Cieri Estrada</h2>
+		</div>
+		<div class="elemento" id="small" style="background-color: #935db1;">
+			<h1>Qué bonito</h1>
+			<p>❝Qué bonito es cuando sin mencionar una sola palabra alguien te recuerda su cariño, con una rosa, una sonrisa, una mirada, un gesto bonito, un beso improvisado o un abrazo repentino en medio del silencio....
+				Y qué bonito es que te tomen de la mano y te devuelvan la calma cuando crees que no hay salida. Qué bonito son esos detalles que no se compran con dinero.❞ </p>
+			<h2>Irene T. Gómez</h2>		
+		</div>
+		<div class="elemento" id="small" style="background-color: #fb8d39;">
+			<h1>Vivos en el momento presente</h1>
+			<p>❝Somos muy buenos preparándonos para vivir, pero no muy buenos viviendo. Sabemos cómo sacrificar diez años por un diploma, y estamos dispuestos a trabajar muy duro para conseguir un empleo, un coche, una casa, y así sucesivamente. Pero tenemos dificultad para recordar que estamos vivos en el momento presente, el único momento que hay para nosotros de estar vivos.❞ </p>
+			<h2>Thich Nhat Hanh</h2>					
+		</div>
+		<div class="elemento" id="small" style="background-color: #df5352;">
+			<h1>❝ ❞</h1>
+			<p>❝Eres mi todo, mi más, mi siempre. ❞ </p>				
+		</div>
+		<div class="elemento" id="small" style="background-color: #ff4076;">
+			<h1>❝ ❞</h1>
+			<p>❝Cómo va a ser pecado, si tocamos el cielo juntos. ❞ </p>				
+		</div>
+		<div class="elemento" id="small" style="background-color: #ad4d30;">
+			<h1>❝ ❞</h1>
+			<p>❝No quiero pelear contigo, quiero pelear por ti. ❞ </p>				
+		</div>
+		<?php 
+				require './php/aceptadas.php';
+
+				$datos = mostrarpublicacion();
+				print_r($datos);
+				$fechactual = $datos[1];
+
+				foreach ($datos[0] as $key => $publicacion) {
+					if (strtotime($fechactual) >= strtotime('24 hour', strtotime($publicacion[4]))) {
+					echo '
+					<div class="elemento" id="small" style="background-color: '.$publicacion['codigo'].'">
+						<h1>❝'.$publicacion['titulo'].'❞</h1>
+						<p>❝'.$publicacion['contenido'].'❞ </p>
+						<h2>'.$publicacion['autor'].'</h2>				
+					</div>';							
+					}
+
+				}
+		?>
+
+	</div>
+
+	<div class="container-fluid" id="creadores-1">
+		<div class="container mt-5" id="creadores-2">
+			<div class="">
+				<h1>¿Creamos una publicación?</h1>
+			</div>
+			<form action="./php/publicaciones.php" method="POST">
+				<div class="row mb-3">
+					<div class="col-sm-12 col-md-6">
+						<input type="text" class="form-control" id="autor" placeholder="Autor" name="autor">
+					</div>
+					<div class="col-sm-12 col-md-6">
+						<input type="text" class="form-control" id="titulo" placeholder="Titulo" name="titulo">
+					</div>
+				</div>
+				<div class="row mb-3">
+					<div class="col-sm-12 mb-3">
+						<input type="text" class="form-control" id="email" placeholder="Email" name="email">
+					</div>
+					<div class="col-sm-12">
+						<textarea class="form-control" id="contenido" placeholder="Contenido" name="contenido" rows="3"></textarea>
+					</div>
+				</div>
+				<div class="row mb-3">
+					<?php 
+						require './php/colores.php';
+
+						$colores = render();
+
+					?>
+					<div class="col-sm-12">
+						<select name="cars" class="custom-select">
+							<option value="0" selected>Color</option>
+							<?php 
+								foreach ($colores as $key => $color) {
+									echo '<option value="'.$color[0].'">'.$color[1].'</option>';
+								}
+							?>
+						</select>
+						<input type="number" name="color" hidden id="color-1">
+						<button type="submit" class="btn btn-block mt-3">Publicar</button>							
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+
+	<hr>
+
+	<footer class="container" id="footer" data-aos="fade-up" data-aos-duration="1000">
+		<div class="row">
+			<div class="col-sm-12">
+				<h1>Demuestra y crea tu propio universo.<i class="fas fa-rocket"></i></h1>
+			</div>
+		</div>
+		<div class="row align-items-center">
+			<div class="col-sm-12 col-md-6 col-lg-4">
+				<div class="row">
+					<div class="col-sm-12" >
+						<a href="https://www.facebook.com/kalashblog/" class="pe-1 text-light"><i class="fab fa-facebook"></i></a>
+						<a href="https://www.instagram.com/kalashblog/" class="pe-1 text-light"><i class="fab fa-instagram"></i></a>
+						<a href="https://co.pinterest.com/kalashblog/_saved/" class="pe-1 text-light"><i class="fab fa-pinterest"></i></a>
+						<a href="https://kalashmyblog.tumblr.com" class="pe-1 text-light"><i class="fab fa-tumblr"></i></a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-sm-12">
+						<a href="" class=""><i class="fas fa-home"></i> Medellín - Colombia</a><br>
+						<a href="" class=""><i class="fas fa-phone"></i> +57 311-344-7976</a><br>
+						<a href="" class=""><i class="fas fa-envelope-open"></i> kalashmyblog@gmail.com</a><br>	
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-12 col-md-6 col-lg-4">
+				<form action="./php/suscribirse.php" method="POST">
+					<div class="form-group">
+						<label for="nombre">Nombre</label><br>
+						<input type="text" name="nombre" required>
+					</div>
+					<div class="form-group">
+						<label for="correo">Email</label><br>
+						<input type="email" name="correo"  required>
+					</div>
+					<button type="submit" class="btn btn-block">Suscribirse</button>
+				</form>
+			</div>
+			<div class="col-sm-12 col-md-6 col-lg-4" style="display: flex; justify-content: center; align-items: center;">
+				<a href="">© 2021 by Kalash.</a>
+			</div>
+		</div>
+	</footer>
+
+	<style>
+		body {
+			background-color: #000000;
+		}
+
+	</style>
+	
+	<script src="http://code.jquery.com/jquery-3.4.1.min.js"></script>
+
+    <script src="./animaciones/anijs-master/dist/anijs-min.js"></script>
+    <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script>
+		<script type="text/javascript">
+            $(document).ready(function(){
+            $('select').on('change',function(){
+            id_color = $(this).find('option:selected').val();
+            $('#color-1').val(id_color);
+            console.log('select');
+            })
+            console.log('carge');
+            })
+		</script>        
+		<script>
+		  AOS.init();
+		</script>
+		<script src="https://unpkg.com/masonry-layout@4/dist/masonry.pkgd.min.js"></script>
+		<script>
+			var container = document.querySelector('.contenedor');
+			var msnry = new Masonry( container, {
+				// options
+				itemSelector: '.elemento'
+			});
+		</script>
+			
+</body>
+
+</html>
